4311d963c2ec2296d949b73101d787e2d7529576
Seriki Walter Yakub <sandraisom@icloud.com>
2026-07-01 19:54:29 +0100

    Update and rename userscript/.user.js to .user.js

--- .user.js
+++ .user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Demo user script
 // @description  I am a demo user script that you can safely delete (add any files to this folder and I will no longer automatically generate)
-// @author       Userscripts
+// @author       seriki walter yakub 
 // @version      0.0.1
 // @match        *://*/*
 // @grant        none
